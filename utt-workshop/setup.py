270bc5442d480f5d711570fde139c817390c74bf
Adja Awa Traoré <adjaawa1998@outlook.fr>
2021-12-09 14:38:45 +0100

    packages

--- utt-workshop/setup.py
+++ utt-workshop/setup.py
@@ -1,10 +1,22 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='Test',
-    author='utt',
-    license='',
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Workshop_Test",
+    version="0.1.0",
+    author="UTT",
+    author_email="utt",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src/api"},
+    packages=setuptools.find_packages(where="src/api"),
+    python_requires=">=3",
 )
